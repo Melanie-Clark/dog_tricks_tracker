a698f10c7c8741689f7360c9241677fbd1235851
Melanie Clark <melaniefclark@gmail.com>
2025-05-18 16:20:12 +0100

    format updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,17 @@
 class SessionsController < ApplicationController
-  def new 
+  def new
   end
 
   def create
     @user = User.find_by_email(params[:session][:email])
-    
+
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to '/', allow_other_host: true
+      redirect_to "/", allow_other_host: true
       puts "Success - User already registered"
     else
-      redirect_to '/login', allow_other_host: true
+      redirect_to "/login", allow_other_host: true
       puts "User not registered or incorrect password"
-    end 
-  end 
-
+    end
+  end
 end
