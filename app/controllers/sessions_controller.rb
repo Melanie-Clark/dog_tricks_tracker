c9608e6b68ea037fa997421b209bacb0f73f21a7
Melanie Clark <melaniefclark@gmail.com>
2025-05-18 16:17:45 +0100

    session controller added

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new 
+  end
+
+  def create
+    @user = User.find_by_email(params[:session][:email])
+    
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to '/', allow_other_host: true
+      puts "Success - User already registered"
+    else
+      redirect_to '/login', allow_other_host: true
+      puts "User not registered or incorrect password"
+    end 
+  end 
+
+end
