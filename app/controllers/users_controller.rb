0486ffda3fb87d4633456bb31d8668cff9a0bc18
Melanie Clark <melaniefclark@gmail.com>
2025-05-22 21:52:00 +0100

    users_controller flash alert messaging and rendering updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,23 +5,20 @@ class UsersController < ApplicationController
 
   # checks if user already exists, if not saves to db
   def create
-    if User.exists?(email: user_params[:email])
-      flash[:alert] = "A user with that email already exists"
-      redirect_to "/register"
+    @user = User.new(user_params)
+    
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
     else
-      @user = User.new(user_params)
-      if @user.save
-        session[:user_id] = @user.id
-        redirect_to "/"
-      else
-        flash[:alert] = "There was a problem creating your account"
-        redirect_to "/register"
-      end
+      # This will show all errors including uniqueness if email taken
+      flash.now[:alert] = @user.errors.full_messages.to_sentence
+      render :new
     end
   end
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
